45fc35a12c1bfaca24712c60730c5bdffffb2aba
Mirko Maggiore <81267022+Meerko@users.noreply.github.com>
2023-10-03 19:32:10 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,20 +6,9 @@ function Footer() {
     <div className=' mt-auto w-screen flex flex-col items-center justify-center pb-10'>
       <div className='flex flex-row items-center justify-between max-w-2xl w-full mx-auto'>
 
-        <a href="https://t.me/ICP_Italia" target='_blank' className='text-white flex flex-row items-center gap-2'>
-          <img src='/telegram.png' alt="Telegram" className='h-[24px]' />
-          ICP Italia
-        </a>
-
-        <a href="https://dfinity.org/" target='_blank' className='text-white flex flex-row items-center gap-2'>
-          <img src='/dfinity_logo.png' alt="DFINITY" className='h-[24px]' />
-          DFINITY Foundation
-        </a>
-
-
-        <a href="https://internetcomputer.org/" target='_blank' className='text-white flex flex-row items-center gap-1'>
-          <img src='/dfinity_logo.png' alt="DFINITY" className='h-[24px]' />
-          Internet Computer
+        <a href="https://t.me/Web3NightLive" target='_blank' className='text-white flex flex-row items-center gap-2'>
+          <img src='/telegram.png' alt="Web3 Night Live Telegram Channel" className='h-[24px]' />
+          WEB3 NIGHT LIVE
         </a>
 
       </div>
