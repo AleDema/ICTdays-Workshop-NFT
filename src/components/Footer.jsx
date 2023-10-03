a480c3884cb39c8ecc736800b16b886cb7b38961
Mirko Maggiore <81267022+Meerko@users.noreply.github.com>
2023-10-03 20:46:09 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
 
   return (
     <div className=' mt-auto w-screen flex flex-col items-center justify-center pb-10'>
-      <div className='flex flex-row items-center justify-between max-w-2xl w-full mx-auto'>
+      <div className='flex flex-row items-center justify-center max-w-2xl w-full mx-auto'>
 
         <a href="https://t.me/Web3NightLive" target='_blank' className='text-white flex flex-row items-center gap-2'>
           <img src='/telegram.png' alt="Web3 Night Live Telegram Channel" className='h-[24px]' />
